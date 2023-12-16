cd3e4b4384a9f756ed86c7aa92836071dbbbf929
joel <joeljoww@gmail.com>
2023-12-16 22:39:05 +0530

    url update

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ mongoose.connect(
 ).then(()=>console.log("Database connected"))
 .catch((err) => {
     console.log(err);
-})
+})                                                          
 
 const corsOptions = {
-    origin: ['http://localhost:3000','http://localhost:5173',"https://user-manage-front-5ss81l7ot-joel-georges-projects.vercel.app", "https://user-management-2sdb.onrender.com" ],
+    origin: ['http://localhost:3000','http://localhost:5173',"https://user-manage-front-end-joel-georges-projects.vercel.app", "https://user-management-2sdb.onrender.com" ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
     credentials: true,
   };
